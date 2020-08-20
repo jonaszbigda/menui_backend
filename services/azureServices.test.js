6aceefeb2fbc34e962235c751fdfacaf9c26e0d6
Jonasz Bigda <jonasz@bankai.pl>
2020-08-20 11:44:32 +0200

    Changes

--- services/azureServices.test.js
+++ services/azureServices.test.js
@@ -1,7 +1,11 @@
 import { removePrefix } from "./azureServices.js";
-import { JsonWebTokenError } from "jsonwebtoken";
 import azureBlob from "@azure/storage-blob";
-jest.mock("@azure/storage-blob");
+
+jest.mock("@azure/storage-blob", () => {
+  return jest.fn().mockImplementation(() => {
+    return { StorageSharedKeyCredential: jest.fn() };
+  });
+});
 
 test("should remove TEMP_ prefix", () => {
   expect(removePrefix("TEMP_abcdef")).toBe("abcdef");
